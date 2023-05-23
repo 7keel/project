e354718bd535589cb79be9167b9a092a3f18264b
7keel <arsen-04@bk.ru>
2023-05-23 20:51:12 +0600

    пока

--- lib/src/screens/newsline/newsline_screen.dart
+++ lib/src/screens/newsline/newsline_screen.dart
@@ -11,6 +11,7 @@ class NewslineScreen extends StatelessWidget {
     "event 6",
     "event 7",
   ];
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,30 +19,52 @@ class NewslineScreen extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "lineScreen",
       home: Scaffold(
-        body: ListView.builder(
-          physics: BouncingScrollPhysics(),
-          itemCount: 3,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      fit: BoxFit.cover,
-                      "assets/images/chad-montano-MqT0asuoIcU-unsplash 1.png",
+        body: Stack(
+          children: [
+            ListView.builder(
+              padding: EdgeInsets.only(top: 80),
+              physics: BouncingScrollPhysics(),
+              itemCount: 6,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
-                    Column(
-                      children: [Text("Esentai Mall")],
-                    )
-                  ],
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          fit: BoxFit.fitWidth,
+                          "assets/images/chad-montano-MqT0asuoIcU-unsplash 1.png",
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            Container(
+             
+              
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+              prefixIcon: IconButton(
+                icon: Icon(Icons.search),
+                onPressed: (){
+                }
+              ),
+                    hintText: "Поиск",
+                    filled: true,
+                    fillColor: Colors.white.withAlpha(235),
+                    border: OutlineInputBorder(),
+                    ) ,
                 ),
               ),
-            );
-          },
+            ),
+          ],
         ),
         // body: ListView(
         //     physics: BouncingScrollPhysics(),
