d3516f1665a51c0cad332314397877cd2e72d383
7keel <arsen-04@bk.ru>
2023-05-24 08:13:35 +0600

    сделал страницу главную

---
+++ lib/src/profile/profile_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      backgroundColor: Color(0xFFF3F4F6),
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: CupertinoColors.white,
+        border: Border(),
+        middle: Text("Профиль"),
+      ),
+      child: Stack(),
+    );
+  }
+}
